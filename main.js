00ce4ccf4bbfcab6cb75361c948118d625588641
Debora Amorim <debora.amorim@sonner.com.br>
2020-04-12 15:18:31 -0300

    Conceito de herança

--- main.js
+++ main.js
@@ -1,11 +1,32 @@
-alert('Teste');
-
-class Teste {
-    metodo (){
-        
+class List{
+    constructor(){
+        this.data = [];
     }
 
-    outro (){
-        
+    add(data){
+        this.data.push(data);
+        console.log(this.data);
     }
 }
+
+class TodoList extends List{
+    
+    constructor(){
+        //está chamando o constructor da classe pai
+        super();
+
+        this.usuario = 'Deb';
+    }
+
+    mostraUsuario(){
+        console.log(this.usuario);
+    }
+}
+
+const MinhaLista = new TodoList();
+
+document.getElementById('novotodo').onclick = function(){
+    MinhaLista.add('Novo todo');
+}
+
+MinhaLista.mostraUsuario();
