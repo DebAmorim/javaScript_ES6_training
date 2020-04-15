f21f4040e663f7ee64a6229d1c30ec4977c16664
Debora Amorim <debora.amorim@sonner.com.br>
2020-04-15 10:43:42 -0300

    Configuração do webpack

--- main.js
+++ main.js
@@ -1,25 +1,3 @@
-/**
- * Object Short Syntax
- * 
- * Uma forma de replicar dados de um objeto em outro
- */
+import { soma } from './funcoes';
 
-
-const nome = 'Deb';
-const idade = 28;
-
-//como seria feito
-// const usuario = {
-//     nome: nome,
-//     idade: idade,
-//     empresa: 'PiButtons',
-// };
-
-//como pode ser feito
-const usuario = {
-    nome,
-    idade,
-    empresa: 'PiButtons',
-};
-
-console.log(usuario);
+console.log(soma(1,2));
