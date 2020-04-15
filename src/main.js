93ed1ffba43551f74178d9d6a92db810f790af28
Debora Amorim <debora.amorim@sonner.com.br>
2020-04-15 14:29:02 -0300

    Promise, async, await

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
-alert('Testando');
+const minhaPromise = () => new Promise((resolve, reject) => {
+    setTimeout(() => { resolve('Ok')}, 2000);
+}); 
+
+const executaPromise = async ()=> {
+    console.log(await minhaPromise());
+    console.log(await minhaPromise());
+    console.log(await minhaPromise());
+}
+
+executaPromise();
